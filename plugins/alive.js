08a8f37a9f5b342d938fccd9a64dfb997d7bb9aa
Shellah9 <neverbesorrynigelbrightshelton@gmail.com>
2024-10-11 13:38:20 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,1 +1,21 @@
+ {cmd , commands} = require('../config')
+const {cmd,commands} = require('../command')
+const { fetchjson } = require('../lib/function')
+cmd({
+    pattern: "ai",
+    desc: "ai chat",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+  let data = await fetchjson('http://chatgpt https://prabath--md-official.vercel.app/api/gptv1?q=${q}')
+  return reply(`${data.data}`)
+}catch{(e){
+console.log(e)
+  reply('${e}')
+
+}
+      })
 
